6850908d9ec16146bfb75bfac4e25c195ca9d8cd
labav <31191175+labav@users.noreply.github.com>
2017-08-22 23:29:27 +0300

    Add files via upload
    Task 1

---
+++ epamhome/src/hometask1/Divide.java
@@ -0,0 +1,13 @@
+package hometask1;
+
+/**
+ * Created by Doomsday Device on 22.08.2017.
+ */
+public class Divide {
+    public static void main(String[] args) {
+        float a = Float.parseFloat(args[0]);
+        float b = Float.parseFloat(args[1]);
+        float c = a / b;
+        System.out.println("Divide: " + c);
+    }
+}
